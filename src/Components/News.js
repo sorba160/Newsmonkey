bb57cfd6dc15d31660b226d65a821de29fbf0710
sorbajit goswami <sorbajitgoswami@gmail.com>
2021-10-28 19:19:15 +0530

    2nd commit

--- src/Components/News.js
+++ src/Components/News.js
@@ -73,7 +73,7 @@ export default class News extends Component {
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4">
-                            <NewsItem key={element.url} title={element.title} description={element.description} imageurl={element.urlToImage} newsurl={element.url} />
+                            <NewsItem key={element.url} title={element.title} description={element.description} imageurl={element.urlToImage} newsurl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                         </div>
 
 
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -3,14 +3,19 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
     render() {
-        let { title, description, imageurl, newsurl } = this.props
+        let { title, description, imageurl, newsurl, author, date, source } = this.props
         return (
             <div>
-                <div className="card" style={{ width: "18rem" }}>
+                <div className="card my-2" style={{ width: "18rem" }}>
                     <img src={imageurl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
+
+                        <h5 className="card-title">{title}<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                            {source}
+                            
+                        </span></h5>
                         <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className="text-success">by {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsurl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
